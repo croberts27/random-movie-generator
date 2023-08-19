6ca1bf8805ca4f7f3d030c051a02baf1f6107cac
Calvin Roberts <you@example.com>
2023-08-18 22:11:35 -0600

    got api to work but doesn't display anything

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const movieContent = document.getElementById("movieContent");
 const movieTitle = document.getElementById("title");
 const movieDescription = document.getElementById("description");
 const movieRating = document.getElementById("rating");
-const button = document.getElementById("button");
+const button = document.getElementById("submitButton");
 
-window.addEventListener("click", async (event) => {
+button.addEventListener("click", async (event) => {
   event.preventDefault();
-  const url = "https://moviesdatabase.p.rapidapi.com/titles/random";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "111c1e463emsh19703b54b872450p10a38bjsncfaa181a66d5",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  };
 
   try {
+    // Retrieve titles from custom list
+    const customList = "random_movies"; // Replace with your actual list name
+    const url = `https://moviesdatabase.p.rapidapi.com/titles?list=${customList}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "111c1e463emsh19703b54b872450p10a38bjsncfaa181a66d5",
+        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+      },
+    };
     const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
+    const data = await response.json();
+
+    // Make sure the 'results' property exists and is an array
+    if (Array.isArray(data.results) && data.results.length > 0) {
+      // Select a random title from the list
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      const randomTitle = data.results[randomIndex];
+
+      // Display the random title's information
+      movieTitle.textContent = `Title: ${randomTitle.titleText}`;
+      movieDescription.textContent = `Description: ${randomTitle.plot}`;
+      movieRating.textContent = `Rating: ${randomTitle.ratingsSummary.averageRating}`;
+      movieContent.classList.remove("invisible");
+    } else {
+      console.log("No results found in the API response.");
+    }
   } catch (error) {
     console.error(error);
   }
